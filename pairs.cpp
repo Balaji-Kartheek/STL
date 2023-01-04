90bf6459c0ebd05382ed541d84785760d86dfbd4
Balaji-Kartheek <tbalaji8822gmail.com>
2023-01-04 15:54:41 +0530

    stl

---
+++ pairs.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    pair<int,int> p = {1,2};
+
+    cout<<p.first<<" "<<p.second<<endl;
+
+    // more than 2 ints
+
+    pair<int,int> arr[] = {{1,2},{10,52},{20,40}};
+
+
+    cout<<arr[1].second<<endl;
+
+
+    return 0;
+
+}
